4d1fecb13aa5e0a0e5aa64084e6670c9aba803b3
Rizal Rio Andrian <rizalrio1357@gmail.com>
2024-05-13 23:34:22 +0700

    upload assets

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var owl = $(".owl-carousel");
+
+owl.owlCarousel({
+  items: 2,
+  loop: true,
+  margin: 10,
+  autoplay: true,
+  autoplayTimeout: 1000,
+  autoplayHoverPause: true,
+  nav: true,
+});
